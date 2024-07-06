cc20b438ae84b1fa1411861ff3b28f9c09a1724f
Victor Marreiros <v.marreiros1@gmail.com>
2024-07-06 16:26:04 -0300

    First Test - Playwright

---
+++ tests/leads.spec.js
@@ -0,0 +1,12 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('deve cadastrar um lead na lista de espera', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.click('//button[text()="Aperte o play... se tiver coragem"]');
+
+  await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click()
+
+  await page.waitForTimeout(10000)
+});
