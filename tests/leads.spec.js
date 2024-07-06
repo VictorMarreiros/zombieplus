e6616e284293481021fdf1f47b2d984e613bcc97
Victor Marreiros <v.marreiros1@gmail.com>
2024-07-06 17:17:41 -0300

    Update first test

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -4,7 +4,11 @@ const { test, expect } = require('@playwright/test');
 test('deve cadastrar um lead na lista de espera', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  await page.getByRole('button', { name: /Aperte o play/ }).click()
+  await page.click('//button[text()="Aperte o play... se tiver coragem"]')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Victor Teste')
+
+  await page.getByPlaceholder('Seu email principal').fill('victor.test@gmai.com')
 
   await page.waitForTimeout(10000)
 });
