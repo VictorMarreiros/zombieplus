c7b8296e4aaae42d0342b47dd1b8e843e96c5ff2
Victor Marreiros <v.marreiros1@gmail.com>
2024-07-07 11:54:41 -0300

    CT03 e CT04: Não deve cadastrar um lead com nome ou email em branco

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -45,3 +45,42 @@ test('não deve cadastrar um lead com formato de email incorreto', async ({ page
 
   await page.waitForTimeout(10000)
 });
+
+
+test('não deve cadastrar um lead com nome em branco', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.click('//button[text()="Aperte o play... se tiver coragem"]')
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu email principal').fill('test@gmail.com')
+
+  await page.getByTestId('modal')
+  .getByText('Quero entrar na fila!').click()
+
+  await expect(page.getByTestId('modal').locator('.alert')).toHaveText('Campo obrigatório')
+
+  await page.waitForTimeout(10000)
+});
+
+test('não deve cadastrar um lead com email em branco', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.click('//button[text()="Aperte o play... se tiver coragem"]')
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Test Teste')
+
+  await page.getByTestId('modal')
+  .getByText('Quero entrar na fila!').click()
+
+  await expect(page.getByTestId('modal').locator('.alert')).toHaveText('Campo obrigatório')
+
+  await page.waitForTimeout(10000)
+});
